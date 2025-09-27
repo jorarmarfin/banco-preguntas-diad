3468f45d94273a183b7e7914ceb252722daf1098
Luis Mayta <luis.mayta@gmail.com>
2025-09-27 17:07:45 -0500

    previo al crud

--- app/Models/Professors.php
+++ app/Models/Professors.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+/*
+ *  $table->string('code')->nullable();
+            $table->string('name')->index();
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->boolean('active')->default(true);
 
+ * */
 class Professors extends Model
 {
-    //
+    protected $fillable = [
+        'code',
+        'name',
+        'email',
+        'phone',
+        'active',
+    ];
+
 }
--- app/Models/QuestionReview.php
+++ app/Models/QuestionReview.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * $table->id();
+ * $table->foreignId('question_id')->constrained()->onDelete('cascade');
+ * $table->foreignId('professor_id')->constrained()->nullOnDelete();
+ * $table->text('comments')->nullable();
+ * $table->date('reviewed_at')->nullable();
+ *
+ * $table->timestamps();
+ */
 class QuestionReview extends Model
 {
-    //
+    protected $fillable = [
+        'question_id',
+        'professor_id',
+        'comments',
+        'reviewed_at',
+    ];
+
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function professor(): BelongsTo
+    {
+        return $this->belongsTo(Professor::class);
+    }
 }
