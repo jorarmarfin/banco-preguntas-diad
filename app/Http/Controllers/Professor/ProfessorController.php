a4b9cfee720a6fe6460898e7819b8b74f9ab6d8a
Luis Mayta <luis.mayta@gmail.com>
2025-09-27 17:03:47 -0500

    preparando crud de docente migraciones

---
+++ app/Http/Controllers/Professor/ProfessorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Professor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfessorController extends Controller
+{
+    public function index()
+    {
+        return view('professors.index');
+    }
+}
---
+++ app/Livewire/Professor/ProfessorLive.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Professor;
+
+use Livewire\Component;
+
+class ProfessorLive extends Component
+{
+    public function render()
+    {
+        return view('livewire.professor.professor-live');
+    }
+}
